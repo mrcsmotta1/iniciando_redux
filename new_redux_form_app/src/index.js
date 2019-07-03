1b56b562ad8a91507ea5a4fb7358f923d9ea5264
Marcos Motta <mrcsmotta1@gmail.com>
2019-07-03 17:50:31 -0300

    Configurando redux e redux form

--- new_redux_form_app/src/index.js
+++ new_redux_form_app/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import cfgStore from './store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = cfgStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
